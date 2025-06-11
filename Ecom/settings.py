54b3c66f136e9004f12e296cf038fc62485a2ac8
mwangiwambui-web <linetwambui559@gmail.com>
2025-06-10 20:05:18 -0700

    Set DEBUG to True and clear ALLOWED_HOSTS; remove Procfile

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sr+6z^*&7#ls0*-)p3aeda86=r6w$pasmx!ko6uaah%$9bn2b_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['escorthub.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
